3859928be7fb36aa7a7e09b81083a0f5c5f8d247
DanielGlonczi <danielglonczi2@gmail.com>
2026-04-21 20:44:38 +0200

    -v3

--- demos/Cube_game/src/scene.c
+++ demos/Cube_game/src/scene.c
@@ -10,14 +10,13 @@ static const int LEVEL_START_X = 1;
 static const int LEVEL_START_Y = 1;
 static const int level[8][8] = {
     {0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 1, 1, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 1, 1, 1, 2, 0, 0, 0},
+    {0, 2, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 1, 1, 2, 0},
+    {0, 0, 0, 2, 0, 1, 0, 0},
     {0, 0, 0, 0, 0, 1, 0, 0},
     {0, 0, 0, 0, 0, 1, 1, 1},
-    {0, 0, 0, 0, 0, 0, 0, 0}
-};
+    {0, 0, 0, 0, 0, 0, 0, 0}};
 
 static bool is_valid_tile(int x, int y)
 {
@@ -28,15 +27,47 @@ static void draw_tile(int x, int y)
 {
     const float tile_z = -2.5f;
     const float half = 0.5f;
+    const float height = 1.0f;
 
     set_material(&(Material){.ambient = {0.10f, 0.30f, 0.45f}, .diffuse = {0.15f, 0.50f, 0.75f}, .specular = {0.10f, 0.10f, 0.10f}, .shininess = 16.0f});
 
     glBegin(GL_QUADS);
+    // Top
     glNormal3f(0.0f, 0.0f, 1.0f);
     glVertex3f((float)x - half, (float)y - half, tile_z);
     glVertex3f((float)x + half, (float)y - half, tile_z);
     glVertex3f((float)x + half, (float)y + half, tile_z);
     glVertex3f((float)x - half, (float)y + half, tile_z);
+    // Bottom
+    glNormal3f(0.0f, 0.0f, -1.0f);
+    glVertex3f((float)x - half, (float)y - half, tile_z - height);
+    glVertex3f((float)x + half, (float)y - half, tile_z - height);
+    glVertex3f((float)x + half, (float)y + half, tile_z - height);
+    glVertex3f((float)x - half, (float)y + half, tile_z - height);
+    // Front
+    glNormal3f(0.0f, 1.0f, 0.0f);
+    glVertex3f((float)x - half, (float)y + half, tile_z);
+    glVertex3f((float)x + half, (float)y + half, tile_z);
+    glVertex3f((float)x + half, (float)y + half, tile_z - height);
+    glVertex3f((float)x - half, (float)y + half, tile_z - height);
+    // Back
+    glNormal3f(0.0f, -1.0f, 0.0f);
+    glVertex3f((float)x - half, (float)y - half, tile_z);
+    glVertex3f((float)x + half, (float)y - half, tile_z);
+    glVertex3f((float)x + half, (float)y - half, tile_z - height);
+    glVertex3f((float)x - half, (float)y - half, tile_z - height);
+    // Left
+    glNormal3f(-1.0f, 0.0f, 0.0f);
+    glVertex3f((float)x - half, (float)y - half, tile_z);
+    glVertex3f((float)x - half, (float)y + half, tile_z);
+    glVertex3f((float)x - half, (float)y + half, tile_z - height);
+    glVertex3f((float)x - half, (float)y - half, tile_z - height);
+    // Right
+    glNormal3f(1.0f, 0.0f, 0.0f);
+    glVertex3f((float)x + half, (float)y - half, tile_z);
+    glVertex3f((float)x + half, (float)y + half, tile_z);
+    glVertex3f((float)x + half, (float)y + half, tile_z - height);
+    glVertex3f((float)x + half, (float)y - half, tile_z - height);
     glEnd();
 }
 
@@ -45,7 +76,8 @@ static void draw_floor_grid(void)
     glDisable(GL_LIGHTING);
     glColor3f(0.45f, 0.45f, 0.45f);
     glBegin(GL_LINES);
-    for (int i = -5; i <= 5; ++i) {
+    for (int i = -5; i <= 5; ++i)
+    {
         glVertex3f((float)i, -5.0f, -2.5f);
         glVertex3f((float)i, 5.0f, -2.5f);
         glVertex3f(-5.0f, (float)i, -2.5f);
@@ -57,20 +89,24 @@ static void draw_floor_grid(void)
 
 static void draw_floor_tiles(void)
 {
-    for (int y = 0; y < LEVEL_HEIGHT; ++y) {
-        for (int x = 0; x < LEVEL_WIDTH; ++x) {
-            if (level[y][x] == 1) {
+    for (int y = 0; y < LEVEL_HEIGHT; ++y)
+    {
+        for (int x = 0; x < LEVEL_WIDTH; ++x)
+        {
+            if (level[y][x] == 1)
+            {
                 draw_tile(x, y);
             }
         }
     }
 }
 
-static void draw_instance(const Model* model, const ModelInstance* inst)
+static void draw_instance(const Model *model, const ModelInstance *inst)
 {
     glPushMatrix();
 
-    if (inst->is_rolling) {
+    if (inst->is_rolling)
+    {
         glTranslatef(inst->pivot_center.x, inst->pivot_center.y, inst->pivot_center.z);
         glRotatef(inst->animation_angle, inst->roll_axis.x, inst->roll_axis.y, inst->roll_axis.z);
         glTranslatef(-inst->pivot_center.x, -inst->pivot_center.y, -inst->pivot_center.z);
@@ -90,9 +126,10 @@ static float minf(float a, float b)
     return (a < b) ? a : b;
 }
 
-static void start_cube_roll(ModelInstance* instance, int dx, int dy, bool will_fall)
+static void start_cube_roll(ModelInstance *instance, int dx, int dy, bool will_fall)
 {
-    if (instance->is_rolling || instance->is_falling) {
+    if (instance->is_rolling || instance->is_falling)
+    {
         return;
     }
 
@@ -103,25 +140,25 @@ static void start_cube_roll(ModelInstance* instance, int dx, int dy, bool will_f
     instance->target_position = (vec3){
         instance->position.x + (float)dx,
         instance->position.y + (float)dy,
-        instance->position.z
-    };
+        instance->position.z};
     instance->pivot_center = (vec3){
         instance->position.x + 0.5f * instance->move_direction.x,
         instance->position.y + 0.5f * instance->move_direction.y,
-        instance->position.z - 0.5f
-    };
+        instance->position.z - 0.5f};
     instance->will_fall = will_fall;
     instance->is_rolling = true;
 }
 
-void move_active_model(Scene* scene, int dx, int dy)
+void move_active_model(Scene *scene, int dx, int dy)
 {
-    if (scene->active_model < 0 || scene->active_model >= scene->model_count) {
+    if (scene->active_model < 0 || scene->active_model >= scene->model_count)
+    {
         return;
     }
 
-    ModelInstance* instance = &scene->instances[scene->active_model];
-    if (!instance->can_move || instance->is_rolling || instance->is_falling) {
+    ModelInstance *instance = &scene->instances[scene->active_model];
+    if (!instance->can_move || instance->is_rolling || instance->is_falling)
+    {
         return;
     }
 
@@ -132,12 +169,13 @@ void move_active_model(Scene* scene, int dx, int dy)
     start_cube_roll(instance, dx, dy, !valid_tile);
 }
 
-void init_scene(Scene* scene)
+void init_scene(Scene *scene)
 {
     scene->model_count = 0;
     scene->active_model = 0;
 
-    if (load_model(&(scene->models[scene->model_count]), "assets/models/cube.obj") == TRUE) {
+    if (load_model(&(scene->models[scene->model_count]), "assets/models/cube.obj") == TRUE)
+    {
         scene->instances[scene->model_count].position = (vec3){(float)LEVEL_START_X, (float)LEVEL_START_Y, -2.0f};
         scene->instances[scene->model_count].rotation = (vec3){0.0f, 0.0f, 0.0f};
         scene->instances[scene->model_count].scale = (vec3){1.0f, 1.0f, 1.0f};
@@ -164,10 +202,10 @@ void init_scene(Scene* scene)
 
 void set_lighting()
 {
-    float ambient_light[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-    float diffuse_light[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-    float specular_light[] = { 0.7f, 0.7f, 0.7f, 1.0f };
-    float position[] = { 0.0f, 4.0f, 4.0f, 1.0f };
+    float ambient_light[] = {0.2f, 0.2f, 0.2f, 1.0f};
+    float diffuse_light[] = {1.0f, 1.0f, 1.0f, 1.0f};
+    float specular_light[] = {0.7f, 0.7f, 0.7f, 1.0f};
+    float position[] = {0.0f, 4.0f, 4.0f, 1.0f};
 
     glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse_light);
@@ -175,11 +213,11 @@ void set_lighting()
     glLightfv(GL_LIGHT0, GL_POSITION, position);
 }
 
-void set_material(const Material* material)
+void set_material(const Material *material)
 {
-    float ambient_material_color[] = { material->ambient.red, material->ambient.green, material->ambient.blue };
-    float diffuse_material_color[] = { material->diffuse.red, material->diffuse.green, material->diffuse.blue };
-    float specular_material_color[] = { material->specular.red, material->specular.green, material->specular.blue };
+    float ambient_material_color[] = {material->ambient.red, material->ambient.green, material->ambient.blue};
+    float diffuse_material_color[] = {material->diffuse.red, material->diffuse.green, material->diffuse.blue};
+    float specular_material_color[] = {material->specular.red, material->specular.green, material->specular.blue};
 
     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_material_color);
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_material_color);
@@ -187,17 +225,20 @@ void set_material(const Material* material)
     glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, &(material->shininess));
 }
 
-void update_scene(Scene* scene, double delta_time)
+void update_scene(Scene *scene, double delta_time)
 {
-    for (int i = 0; i < scene->model_count; ++i) {
-        ModelInstance* instance = &scene->instances[i];
+    for (int i = 0; i < scene->model_count; ++i)
+    {
+        ModelInstance *instance = &scene->instances[i];
 
-        if (instance->is_rolling) {
+        if (instance->is_rolling)
+        {
             float step_angle = minf(180.0f * (float)delta_time, instance->remaining_angle);
             instance->animation_angle += step_angle;
             instance->remaining_angle -= step_angle;
 
-            if (instance->remaining_angle <= 0.0f) {
+            if (instance->remaining_angle <= 0.0f)
+            {
                 instance->is_rolling = false;
                 instance->animation_angle = 0.0f;
                 instance->position = instance->target_position;
@@ -205,24 +246,33 @@ void update_scene(Scene* scene, double delta_time)
                 instance->rotation.y += instance->roll_axis.y * 90.0f;
                 instance->rotation.z += instance->roll_axis.z * 90.0f;
 
-                if (instance->rotation.x < 0.0f) instance->rotation.x += 360.0f;
-                if (instance->rotation.x >= 360.0f) instance->rotation.x -= 360.0f;
-                if (instance->rotation.y < 0.0f) instance->rotation.y += 360.0f;
-                if (instance->rotation.y >= 360.0f) instance->rotation.y -= 360.0f;
-                if (instance->rotation.z < 0.0f) instance->rotation.z += 360.0f;
-                if (instance->rotation.z >= 360.0f) instance->rotation.z -= 360.0f;
+                if (instance->rotation.x < 0.0f)
+                    instance->rotation.x += 360.0f;
+                if (instance->rotation.x >= 360.0f)
+                    instance->rotation.x -= 360.0f;
+                if (instance->rotation.y < 0.0f)
+                    instance->rotation.y += 360.0f;
+                if (instance->rotation.y >= 360.0f)
+                    instance->rotation.y -= 360.0f;
+                if (instance->rotation.z < 0.0f)
+                    instance->rotation.z += 360.0f;
+                if (instance->rotation.z >= 360.0f)
+                    instance->rotation.z -= 360.0f;
 
-                if (instance->will_fall) {
+                if (instance->will_fall)
+                {
                     instance->is_falling = true;
                     instance->fall_velocity = 0.0f;
                 }
             }
         }
 
-        if (instance->is_falling) {
+        if (instance->is_falling)
+        {
             instance->fall_velocity += 9.8f * (float)delta_time;
             instance->position.z -= instance->fall_velocity * (float)delta_time;
-            if (instance->position.z < -7.0f) {
+            if (instance->position.z < -7.0f)
+            {
                 instance->position.x = (float)LEVEL_START_X;
                 instance->position.y = (float)LEVEL_START_Y;
                 instance->position.z = -2.0f;
@@ -236,7 +286,7 @@ void update_scene(Scene* scene, double delta_time)
     }
 }
 
-void render_scene(const Scene* scene)
+void render_scene(const Scene *scene)
 {
     set_material(&(scene->material));
     set_lighting();
@@ -244,7 +294,8 @@ void render_scene(const Scene* scene)
     draw_floor_tiles();
     draw_floor_grid();
 
-    for (int i = 0; i < scene->model_count; ++i) {
+    for (int i = 0; i < scene->model_count; ++i)
+    {
         draw_instance(&(scene->models[i]), &(scene->instances[i]));
     }
 }
@@ -253,9 +304,15 @@ void draw_origin()
 {
     glDisable(GL_LIGHTING);
     glBegin(GL_LINES);
-    glColor3f(1, 0, 0); glVertex3f(0, 0, 0); glVertex3f(1, 0, 0);
-    glColor3f(0, 1, 0); glVertex3f(0, 0, 0); glVertex3f(0, 1, 0);
-    glColor3f(0, 0, 1); glVertex3f(0, 0, 0); glVertex3f(0, 0, 1);
+    glColor3f(1, 0, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(1, 0, 0);
+    glColor3f(0, 1, 0);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 1, 0);
+    glColor3f(0, 0, 1);
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 0, 1);
     glEnd();
     glEnable(GL_LIGHTING);
 }
